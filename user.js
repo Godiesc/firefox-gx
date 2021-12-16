d535f50543230bc202134cc8dfa308e5650961af
GodieGun <diego_sc25@outlook.com>
2021-12-16 00:07:02 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,2 +1,5 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
 user_pref("svg.context-properties.content.enabled", true); 
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
+user_pref("layout.css.backdrop-filter.enabled", true);
