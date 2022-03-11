f0617f45d37890741ee357ba068549c72595025c
GodieGun <diego_sc25@outlook.com>
2022-03-11 13:40:08 -0500

    Update user.js

--- user.js
+++ user.js
@@ -3,3 +3,4 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("layout.css.color-mix.enabled", true);
 user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
 user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("widget.non-native-theme.scrollbar.style", 5);
