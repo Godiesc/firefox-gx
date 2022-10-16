24ec515fef835a7891279117033ebe566bd98a3d
GodieGun <diego_sc25@outlook.com>
2022-10-15 21:36:28 -0500

    Update user.js

--- user.js
+++ user.js
@@ -3,3 +3,4 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("layout.css.color-mix.enabled", true);
 user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
 user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
