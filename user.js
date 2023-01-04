99845e6db7b103ee501935b660c5c57fdde55ceb
GodieGun <diego_sc25@outlook.com>
2023-01-03 22:12:54 -0500

    Update user.js

--- user.js
+++ user.js
@@ -3,10 +3,10 @@
 
 /* Default rules */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", 			     true); 
-user_pref("layout.css.color-mix.enabled", 						     true);
-user_pref("browser.tabs.delayHidingAudioPlayingIconMS",			     0);
-user_pref("layout.css.backdrop-filter.enabled", 				     true);
+user_pref("svg.context-properties.content.enabled", true); 
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
+user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
 
 /*To active container tabs without any extension */
