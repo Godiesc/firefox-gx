94303d25355dbdc1073cc905cc77fd5b97a5938b
GodieGun <diego_sc25@outlook.com>
2023-01-03 22:12:11 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,15 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
-user_pref("svg.context-properties.content.enabled", true); 
-user_pref("layout.css.color-mix.enabled", true);
-user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
-user_pref("layout.css.backdrop-filter.enabled", true);
+/* Source file made available under Mozilla Public License v. 2.0 See the main repository for updates as well as full license text. 
+   https://github.com/Godiesc/opera-gx */
+
+/* Default rules */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", 			     true); 
+user_pref("layout.css.color-mix.enabled", 						     true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS",			     0);
+user_pref("layout.css.backdrop-filter.enabled", 				     true);
 user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+
+/*To active container tabs without any extension */
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("privacy.userContext.longPressBehavior", 2);
