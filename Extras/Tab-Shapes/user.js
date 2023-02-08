3ad2b40983147532595b853e59906ed3640cfad1
GodieGun <diego_sc25@outlook.com>
2023-02-08 16:18:05 -0500

    Update user.js

--- Extras/Tab-Shapes/user.js
+++ Extras/Tab-Shapes/user.js
@@ -3,18 +3,18 @@
 
 /* Default rules */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", 			     true); 
-user_pref("layout.css.color-mix.enabled", 						     true);
-user_pref("browser.tabs.delayHidingAudioPlayingIconMS",			     0);
-user_pref("layout.css.backdrop-filter.enabled", 				     true);
+user_pref("svg.context-properties.content.enabled", true); 
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
+user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
 
 /* Make it all false if you don't want other forms of tabs than default */
-user_pref("userChrome.tab.bottom_rounded_corner",      true);
+user_pref("userChrome.tab.bottom_rounded_corner", true);
 
 /* Make it True (only one) the rule with the form of the tab you want */
-user_pref("userChrome.tab.bottom_rounded_corner.wave",         false);
-user_pref("userChrome.tab.bottom_rounded_corner.australis", 	false);
-user_pref("userChrome.tab.bottom_rounded_corner.chrome",    	true);
-user_pref("userChrome.tab.bottom_rounded_corner.chrome_legacy",false);
-user_pref("userChrome.tab.bottom_rounded_corner.edge",      	false);
+user_pref("userChrome.tab.bottom_rounded_corner.wave",          false);
+user_pref("userChrome.tab.bottom_rounded_corner.australis",     false);
+user_pref("userChrome.tab.bottom_rounded_corner.chrome",        true);
+user_pref("userChrome.tab.bottom_rounded_corner.chrome_legacy", false);
+user_pref("userChrome.tab.bottom_rounded_corner.edge",          false);
