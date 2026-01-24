b47a58adac360243d4d16d0190d58a2ec530e0f8
GodieGun <diego_sc25@outlook.com>
2026-01-24 12:46:43 -0500

    Update user.js
    Remove deprecated preference

--- user.js
+++ user.js
@@ -1,11 +1,10 @@
 /* Source file made available under Mozilla Public License v. 2.0 See the main repository for updates as well as full license text. 
-   https://github.com/Godiesc/opera-gx */
+   https://github.com/Godiesc/firefox-gx */
 
 /* Default rules */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
-user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
 user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("sidebar.revamp.round-content-area", false);
 
