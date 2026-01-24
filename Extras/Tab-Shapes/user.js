eb24b444efecdee2c3734e9199d03aa0cb43578e
GodieGun <diego_sc25@outlook.com>
2026-01-24 13:04:35 -0500

    Remove deprecated preferences

--- Extras/Tab-Shapes/user.js
+++ Extras/Tab-Shapes/user.js
@@ -1,20 +1,22 @@
 /* Source file made available under Mozilla Public License v. 2.0 See the main repository for updates as well as full license text. 
-   https://github.com/Godiesc/opera-gx */
+   https://github.com/Godiesc/firefox-gx */
 
 /* Default rules */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true); 
-user_pref("layout.css.color-mix.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
-user_pref("layout.css.backdrop-filter.enabled", true);
-user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("sidebar.revamp.round-content-area", false);
 
-/* Make it all false if you don't want other forms of tabs than default */
-user_pref("userChrome.tab.bottom_rounded_corner", true);
+/*To active container tabs without any extension */
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("privacy.userContext.longPressBehavior", 2);
 
 /* Make it True (only one) the rule with the form of the tab you want */
 user_pref("userChrome.tab.bottom_rounded_corner.wave",          false);
-user_pref("userChrome.tab.bottom_rounded_corner.australis",     false);
-user_pref("userChrome.tab.bottom_rounded_corner.chrome",        true);
+user_pref("userChrome.tab.bottom_rounded_corner.australis",     true);
+user_pref("userChrome.tab.bottom_rounded_corner.chrome",        false);
 user_pref("userChrome.tab.bottom_rounded_corner.chrome_legacy", false);
 user_pref("userChrome.tab.bottom_rounded_corner.edge",          false);
+
